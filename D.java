cfade729f8d3408cf04181098bdf5a8f693357ab
DanielSantos <danmes12345@gmail.com>
2022-12-04 18:11:28 -0300

    Alteracao na classe B e criacao de D

---
+++ D.java
@@ -0,0 +1,28 @@
+public class D{
+    String D1;
+    int D2;
+  
+    public int getD1(){
+     return D1; 
+    }
+  
+    public int getD2(){
+     return D2; 
+    }
+  
+    public void setD1(int D1){
+     this.D1 = D1; 
+    }
+  
+    public void setD2(int D2){
+     this.D2 = D2; 
+    }
+    
+    public void MD1(){
+      System.out.println("MD1");
+    }
+  
+    public void MD2(){
+      System.out.println("MD2");
+    }
+}
